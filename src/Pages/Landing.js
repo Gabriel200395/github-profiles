559a76ea8f7f8e3ded74fe49fa570e8855bfeb08
gabriel <ugabrielf@gmail.comgit config --global user.name Gabrielgit config --global user.email ugabrielf@gmail.com>
2020-11-18 11:56:18 -0300

    Components Atualizado

---
+++ src/Pages/Landing.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
+import "../Styles/global.css";
+import "../Styles/landing.css";
+
+export default function Landing() {
+  const [users, setUsers] = useState([]);
+  const [repositorios, setRepositorios] = useState([]);
+
+  useEffect(() => {
+    function loading() {
+      Axios.get("https://api.github.com/users/gabriel200395").then((res) => {
+        setUsers(res.data);
+      });
+    }
+    loading();
+  }, []);
+
+  useEffect(() => {
+    function repo() {
+      Axios.get("https://api.github.com/users/gabriel200395/repos").then(
+        (res) => {
+          setRepositorios(res.data);
+        }
+      );
+    }
+    repo();
+  }, []);
+
+  return (
+    <section className="landing">
+      <div className="landing-navbar">
+        <h1 className="Logo">GitHub</h1>
+        <ul>
+          <li>Linkdin</li>
+          <li>GitHub</li>
+          <li>Portfolio</li>
+        </ul>
+      </div>
+      <div className="landing-container-user">
+        <img className="imagem" src={users.avatar_url} alt={users} />
+        <h1 className="titulo">{users.bio}</h1>
+        <ul>
+          <li>Username : {users.login}</li>
+          <li>Repositório : {users.public_repos}</li>
+          <li>Seguidores : {users.followers}</li>
+          <li>Criado : {users.created_at}</li>
+        </ul>
+      </div>
+
+      {/*Repositorios */}
+      <section className="repo">
+        <h1>Repositórios</h1>
+        <div className="landing-container-repo">
+          {repositorios.map((repositorio) => (
+            <div className="landing-repo-card" key={repositorio.id}>
+              <h3>{repositorio.name}</h3>
+              <p className="landing-repo-description">
+                {repositorio.description}
+              </p>
+              <ul className="landing-repo-github">
+                <li>{repositorio.language}</li>
+                <li>{repositorio.updated_at}</li>
+              </ul>
+              <a className="landing-repo-button" href={repositorio.svn_url}>
+                Github
+              </a>
+            </div>
+          ))}
+        </div>
+      </section>
+    </section>
+  );
+}
